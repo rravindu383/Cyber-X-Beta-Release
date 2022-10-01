64a3bd3512a8bd7c79a8a22f33d1c2b1811920b8
rravindu383 <114388637+rravindu383@users.noreply.github.com>
2022-10-02 04:50:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,19 +43,19 @@ global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
+global.owner = ['94761380756']
 global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.botnumber = '94761380756' //Add your Bot Number For Log
+global.botname = '@°|Rᴀᴠɪɴᴅᴜ_ᴏꜰꜰɪᴄɪᴀʟ: ⃦:🚶🏻‍♂️🥀' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "@°|Rᴀᴠɪɴᴅᴜ_ᴏꜰꜰɪᴄɪᴀʟ: ⃦:🚶🏻‍♂️🥀" //ur sticker watermark packname
+global.author = "@°|Rᴀᴠɪɴᴅᴜ_ᴏꜰꜰɪᴄɪᴀʟ: ⃦:🚶🏻‍♂️🥀" //ur sticker watermark author
+global.footer = "@°|Rᴀᴠɪɴᴅᴜ_ᴏꜰꜰɪᴄɪᴀʟ: ⃦:🚶🏻‍♂️🥀"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
